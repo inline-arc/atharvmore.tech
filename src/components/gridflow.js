32087fe9923e8259a770f417b357e9b4708761f2
inline-arc <atharvmore20@gmail.com>
2024-06-12 20:42:43 +0530

    mode and float

--- src/components/gridflow.js
+++ src/components/gridflow.js
@@ -3,6 +3,18 @@ import React from "react";
 export default function Gridflow() {
 
     return (
-<div class="absolute h-full w-full overflow-hidden opacity-50 [perspective:250px]"><div class="absolute inset-0 [transform:rotateX(45deg)]"><div class="inset-0 h-screen animate-move bg-repeat [background-image:linear-gradient(to_right,rgba(0,0,0)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0)_1px,transparent_0)] [background-size:60px_30px] [transform-origin:100%_0_0]"></div></div><div class="absolute inset-0 bg-gradient-to-b from-white to-transparent to-60%"></div></div>
-    );
+        <>
+        <div class="absolute h-full w-full overflow-hidden opacity-50 [perspective:250px]">
+            <div class="absolute inset-0 [transform:rotateX(45deg)]">
+                <div class="inset-0 h-screen animate-move bg-repeat [background-image:linear-gradient(to_right,rgba(0,0,0)_2px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0)_2px,transparent_0)] [background-size:60px_30px] [transform-origin:100%_0_0]">
+                </div>
+        </div>
+        <div className="flex justify-between items-center">
+        <img src="https://i.pinimg.com/564x/39/90/fe/3990fee0cb3d6cb1c484f85bd6c0489b.jpg" class="h-60 m-6 rotate-45 rounded-full" alt="background"/>
+        <img src="" class="h-60 flex-none"  alt="lol"/>
+        <img src="https://i.pinimg.com/564x/fb/c7/3c/fbc73c326c6d44974528aa369881919e.jpg" class="h-52 m-16 rounded-sm"  alt="none"/>
+        </div>
+        </div>
+        </>
+);
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,14 +4,18 @@ import "../font/Quinquefive-ALoRM.ttf";
 
 export default function Navbar() {
   return (
-    <div className="App flex justify-between items-center h-16 bg-[#80E93D] text-black relative border-0 m-9 rounded-2xl drop-shadow-xl">
+    <div className="App flex justify-between items-center h-16 bg-[#80E93D] text-black border-0 m-9 rounded-2xl drop-shadow-xl sticky">
       <a href="/" className="pl-8">Atharv x MORE</a>
-      <div className="pr-8">
+      <div className="pr-12">
         <a href="/contact" className="p-4 ">About</a>
         <a href="/projects" className="p-4">Projects</a>
-        <a href="/database" className="p-4">BreakOut</a>
-        <a href="/database" className="p-4 bg-white rounded-full">Contant</a>
-      </div>
+        <a href="/BreakOut" className="p-4">BreakOut</a>
+        <a href="/Contact" className="p-4 bg-white rounded-full">Contant</a>
     </div>
+    <input class="hidden" type="checkbox" id="toggle_1" value="1"/>
+	<label class="flex items-center relative w-12 border-black h-6 p-1 m-10 rounded-full border-2" for="toggle_1">
+		<span class="w-4 h-4 bg-black rounded-full"></span>
+	</label>
+  </div>
   );
 }
